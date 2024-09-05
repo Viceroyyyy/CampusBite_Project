6ab6fb4a23f00b7609c015c16cbf5b69ede3173a
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-09-06 01:28:11 +0530

    React Router Dom added for pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import NavBar from './components/NavBar/NavBar'
-
+import { Link,Routes,Route } from 'react-router-dom'
 const App = () => {
   return (
     <div className='app'>
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/cart" element={<Cart/>}></Route>
+        <Route path="/order" element={<PlaceOrder/>}></Route>
+        {/* <Route path="/" element={<Home/>}></Route> */}
+      </Routes>
     </div>
   )
-}
+}  
 
 export default App
