e0a3d2ee14ed600efad4c3234d376e48479d5196
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-09-06 20:18:11 +0530

    Added Header section along with styling

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food here:-</h2>
+        <p>Welcome to Campus Bite – Your One-Stop Solution for Fast and Fresh Food on Campus! Order your favorite meals from the campus canteens with just a few clicks. Satisfy your cravings and enjoy convenient, hassle-free delivery.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
