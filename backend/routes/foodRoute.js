675ab47899ecdd4377378ee8aaec57d408d15815
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-09-12 23:48:47 +0530

    Created a route to use get and post method

---
+++ backend/routes/foodRoute.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { addFood } from "../controllers/foodController.js";
+import multer from 'multer'; //We will create image storage system
+
+
+const foodRouter = express.Router();
+//Using this Router we can make get method post method
+
+//Image Storage Engine
+
+
+
+
+
+export default foodRouter;
