2297297650af8c6432a1911f421e70dea012d915
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-09-12 23:48:05 +0530

    Created and API to add foodItems to the Schema

---
+++ backend/controllers/foodController.js
@@ -0,0 +1,10 @@
+import foodModel from "../models/foodModel.js";
+import fs from 'fs'; //fs file system that is prebuilt in the node.js
+
+//add food item
+//Controller function to add foodItem and using this we will create a Route
+const addFood = async (req,res)=> {
+
+}
+
+export {addFood}
