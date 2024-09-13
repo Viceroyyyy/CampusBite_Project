a2d56312186bab3d7afebfcf25885e2e99684fb3
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-09-13 23:21:14 +0530

    Updated Food Router

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -8,8 +8,16 @@ const foodRouter = express.Router();
 
 //Image Storage Engine
 
+const storage = multer.diskStorage({
+    destination: "uploads",
+    filename:(req,file,cb)=>{ //cb stands for callback 
+        return cb(null,`${Date.now()}${file.originalname}`); //returns the curret timestamp we use that so that our file becomes unique everytime it is created and this is stored in uploads folder  
+    }
+})
 
+const upload = multer({storage: storage})
 
+foodRouter.post("/add",upload.single("image"),addFood)
 
 
 export default foodRouter;
