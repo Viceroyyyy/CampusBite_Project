46a15d432383d4ad1f29e993d963a3a4c9ce2d56
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-09-15 22:09:37 +0530

    Routes added to side bar buttons

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import Add from './pages/Add/Add'
+import List from './pages/List/List'
+import Orders from './pages/Orders/Orders'
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/orders" element={<Orders />} />
+          
+        </Routes>
+      </div>
     </div>
   )
 }
+/* Key Differences between Link and NavLink tag of react-router-dom:
+
+	•	Styling: Link is just a basic anchor tag for navigation, while NavLink allows you to automatically apply active styles when a route is active.
+	•	Use Case: Link is suitable for regular navigation, and NavLink is best for menus where highlighting the active link is desired. */
 
 export default App
