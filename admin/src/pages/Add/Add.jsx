e1cf0bf643a9203af4abafa7daa7023ae00394ce
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-09-17 14:08:48 +0530

    Server Url props added

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -4,11 +4,11 @@ import { assets } from '../../assets/assets'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 
-const Add = () => {
+const Add = ({url}) => {
 
-const url = "http://localhost:4000";
+// const url = "http://localhost:4000";
 
-const [image,setImage] =  useState(false);
+const [image,setImage] =  useState(null);
 const [data,setData] = useState({
     name: "",
     description:"",
