602a2fdc459e059268cf131efcad6f22a79b5007
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-09-20 08:49:07 +0530

    Cart API connected to the frontend

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -15,11 +15,14 @@ const StoreContextProvider = (props) => {
 
   const[food_list, setFoodList] = useState([]);
 
-  const addToCart = (itemId) => {
+  const addToCart = async (itemId) => {
     if (!cartItems[itemId]) {
       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
     } else {
       setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+    if(token){
+      await axios.post(url+"/api/cart/add",{itemId},{headers:{token}})
     }
   };
   //To check the output in console whether the item is added or not..
@@ -36,13 +39,14 @@ const StoreContextProvider = (props) => {
   }
 
 
-  useEffect(() => {
+  useEffect(() => { //This to update the page whenever it is reloaded.
     
     async function loadData(){
       await fetchFoodList();
 
       if(localStorage.getItem('token')){
         setToken(localStorage.getItem('token'));
+        await loadCartData(localStorage.getItem('token'));
       }
     }
     loadData();
@@ -61,8 +65,19 @@ const StoreContextProvider = (props) => {
 
 
 
-  const removeFromCart = (itemId) => {
+  const removeFromCart = async (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+
+    if(token)
+    {
+      await axios.post(url+'/api/cart/remove',{itemId},{headers:{token}});
+    }
+  };
+
+
+  const loadCartData = async (token) => {
+    const response = await axios.post(url+'/api/cart/get',{},{headers:{token}});
+    setCartItems(response.data.cartData);
   };
 
   //  2. State (cartItems):
