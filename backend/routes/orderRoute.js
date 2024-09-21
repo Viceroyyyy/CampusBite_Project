260f78f341e75b30917c2f5a8c58d31afb518ec4
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-09-21 21:37:40 +0530

    OrderRoute reated

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,9 @@
+import { placeOrder } from "../controllers/orderController.js";
+import authMiddleware from "../middleware/auth.js";
+import express from "express";
+
+const orderRouter = express.Router();
+
+orderRouter.post('/place',authMiddleware,placeOrder);
+
+export default orderRouter;
