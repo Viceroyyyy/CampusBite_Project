9268d3eaf7443ee481ee1d9efefcf7c01d996db1
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-09-23 13:00:32 +0530

    orderRoute created for place Order and userOrder

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,9 +1,10 @@
-import { placeOrder } from "../controllers/orderController.js";
+import { placeOrder, userOrder } from "../controllers/orderController.js";
 import authMiddleware from "../middleware/auth.js";
 import express from "express";
 
 const orderRouter = express.Router();
 
 orderRouter.post('/place',authMiddleware,placeOrder);
+orderRouter.post('/userorders',authMiddleware,userOrder);
 
 export default orderRouter;
