4f602adcebfe09a7fc81759139f932896d320a0c
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-09-23 12:59:52 +0530

    Database Model for Orders

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -6,7 +6,8 @@ const orderSchema = new mongoose.Schema({
     amount: {type: Number, required: true},
     status: {type: String, default: "Food Processing"},
     date: {type: Date, default:Date.now()},
-    payment : {type: Boolean, default:false}
+    payment : {type: Boolean, default:false},
+    razorpayId : {type: String},
 });
 
 const orderModel = mongoose.models.order || mongoose.model('order',orderSchema);
