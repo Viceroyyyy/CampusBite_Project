561dab30dc311c886eb173092ad8f5b5d1597e8c
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-09-24 06:44:15 +0530

    Applying functionality to track Order Button

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -44,7 +44,7 @@ const MyOrders = () => {
                    <p>Items: {order.items.length}</p>
                    </div>
                    <p><span>&#x25cf;</span><b>{order.status}</b></p>
-                   <button>Track Order</button>
+                   <button onClick={fetchOrders}>Track Order</button>
                 </div>
             )
         })}
